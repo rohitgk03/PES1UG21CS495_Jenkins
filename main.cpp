3d0435d8ca4c0b80617364f00f062cd314658658
Rohit G Kulkarni <139968764+rohitgk03@users.noreply.github.com>
2024-03-11 19:05:56 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,8 @@ int main(){
     int i=1;
     int a[]={0,1,0,0,0,0,0,0,0,0};
     for(int i=1;i<8;i++){
-        cout<<a[i]<<endl;
+        cout<<a[i]<<end;
         a[i+1]=a[i]+a[i-1];
     }
 
 }
-      
-  
